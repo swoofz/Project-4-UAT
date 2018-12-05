f5ea688d3a4509ac32f0c75ab1aa13f663748fd0
swoofz <jordanhall.1995@yahoo.com>
2018-12-04 21:03:25 -0800

    Added set up scripts

---
+++ Super UAT Brothers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;             // Create Singleton
+
+    void Awake() {
+        if (instance != null) {     // If the is an Game Manager instance
+            Destroy(gameObject);    // Destory new one
+        } else {                            // Otherwise
+            instance = this;                // Set instance to this Game Manager
+            DontDestroyOnLoad(gameObject);  // Don't destory this when going to new scenes
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
