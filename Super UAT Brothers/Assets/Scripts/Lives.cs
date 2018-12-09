9688c8c89ba9a7ed0905decee3fa8bedc8c36a93
swoofz <jordanhall.1995@yahoo.com>
2018-12-09 11:34:10 -0800

    Add Lives tracker to UI

---
+++ Super UAT Brothers/Assets/Scripts/Lives.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Lives : MonoBehaviour {
+
+    private Text lives;
+
+	// Use this for initialization
+	void Start () {
+        lives = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lives.text = "Lives: " + GameManager.instance.playerLives;
+	}
+}
